2bd14097a2c3b26c1c34bd34a2e4b690bc6fb1db
thanhpm <thanhPM210@gmail.com>
2025-10-31 00:21:26 +0700

    update

--- scripts/convert.js
+++ scripts/convert.js
@@ -1,4 +1,4 @@
-if(GithubDrive!=null)exit(0);
+if(GithubDrive==null){
 console.log("convert to dir")
 GithubDrive =new drive(new URLSearchParams(window.location.search));
 /** global shareValue */
@@ -10,9 +10,28 @@ GithubDrive.GetTree()
         return j.tree;
     })
     .then(tree=>{
+        console.log("tree");
         console.log(tree);
-        for(let i=0;i<t.lenght;t++){
+        console.log(tree.length);
+        GithubDrive.content={};
+        for(let i=0;i<tree.length;i++){
             let tmp=GithubDrive.content;
             let path=tree[i].path.split('/');
+            console.log(path);
+            let j=0;
+            for(j=0;j<path.length-1;j++){
+                if(tmp[path[j]]==undefined){
+                    tmp[path[j]]={};
+                }
+                tmp=tmp[path[j]];
+                // console.log(j);
+            }
+            console.log(`${j},${i}`);
+            console.log(tree[i]);
+            tmp[path[j]]=new Object(tree[i]);
         }
+        sessionStorage.setItem("drive",JSON.stringify(GithubDrive));
+        // console.log(sessionStorage.getItem("drive"));
     })
+}
+console.log("loaded from session");
