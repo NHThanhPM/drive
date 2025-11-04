b47c3ad8978fe24f727739de13f69545f2dfaf7a
Nguyễn Hữu Thành <thanhpm210@gmail.com>
2025-11-04 03:50:55 +0000

    refactor

--- scripts/init.js
+++ scripts/init.js
@@ -1,38 +1,72 @@
-//GET https://api.github.com/repos/{owner}/{repo}/git/trees/{branch}?recursive=1
+//. GET https://api.github.com/repos/{owner}/{repo}/git/trees/{branch}?recursive=1
+let parameters=new URLSearchParams(window.location.search);
+let pif=parameters.get("path");
+let path=pif.split('/');;
 class drive {
-    owner;
-    repo;
-    branch;
+    RepoInfo={
+        owner: "",
+        repo:"",
+        branch:"",
+    };
     content={};
+    lenght;
     constructor(params) {
-        this.owner = params.get("owner");
-        this.repo = params.get("repo");
-        this.branch = params.get("branch");
-        if(this.branch==null){
+        // path= params.get("path").split('/');
+        if(path.length<3)return;
+        this.RepoInfo.owner = path[0];
+        this.RepoInfo.repo = path[1];
+        this.RepoInfo.branch = path[2];
+        if(this.RepoInfo.branch in ["",null,undefined]){
             console.debug("set default branch");
-            this.branch="main";
+            this.RepoInfo.branch="main";
         }
-        console.log(`dir: ${this.owner}>${this.repo}>${this.branch}`)
+        this.lenght=this.RepoInfo.owner.length+this.RepoInfo.repo.length+this.RepoInfo.branch.length+3;
+        console.log(`Drive info:${this.RepoInfo.owner},${this.RepoInfo.repo},${this.RepoInfo.branch}`);
     }
     GetTree() {
-        return fetch(`https://api.github.com/repos/${this.owner}/${this.repo}/git/trees/${this.branch}?recursive=1`);
+        return fetch(`https://api.github.com/repos/${this.RepoInfo.owner}/${this.RepoInfo.repo}/git/trees/${this.RepoInfo.branch}?recursive=1`);
     }
     download_url= path=>{
-        return `https://raw.githubusercontent.com/${this.owner}/${this.repo}/${this.branch}/${path}`;
+        return `https://raw.githubusercontent.com/${this.RepoInfo.owner}/${this.RepoInfo.repo}/${this.RepoInfo.branch}/${pif.substring(this.lenght)}`;
     };
 }
-var GithubDrive=sessionStorage.getItem("drive");
-if(GithubDrive!=undefined){
-    GithubDrive=JSON.parse(GithubDrive);
+function GetInfoFromSession(){
+    let tmp=sessionStorage.getItem("drive_info");
+    if(tmp==null || tmp==undefined)return null;
+    return JSON.parse(tmp);
 }
-console.log("kgkkhjk:")
-console.log(GithubDrive);
-// function debug(){
-//     const temp=new drive(new URLSearchParams(window.location.search));
-//     temp.GetTree()
-//         .then(j => j.json())
-//         .then(r=>console.log(r))
-// }
-// debug()
-let Container=document.getElementById("Files");
-// Container.id="Files";
+function ConvertListToTree(list){
+    let tree={};
+    for(let i=0;i<list.length;i++){
+        let item=list[i];
+        let parts=item.path.split('/');
+        let current=tree;
+        for(let j=0;j<parts.length-1;j++){
+            if(current[parts[j]]==undefined){
+                current[parts[j]]={};
+            }
+            current=current[parts[j]];
+        }
+        current[parts[parts.length-1]]=item;
+    }
+    return tree;
+}
+let GithubDrive=new drive(parameters);
+if(GithubDrive.RepoInfo==GetInfoFromSession()){
+    console.log("Load drive info from session storage");
+    GithubDrive.content=JSON.parse(sessionStorage.getItem("drive_content"));
+}
+else{
+    sessionStorage.setItem("drive_info",JSON.stringify(GithubDrive.RepoInfo));
+    console.log("Fetch drive info from github api");
+    GithubDrive.GetTree()
+    .then(response=>response.json())
+    .then(data=>{
+        console.log("Fetched data:");
+        console.log("convert list to tree");
+        GithubDrive.content=ConvertListToTree(data.tree);
+        console.log(GithubDrive.content);
+        sessionStorage.setItem("drive_content",JSON.stringify(GithubDrive.content));
+        // window.location.reload();
+    })
+}
